92deebd9cf46e0d354d3c016f6119c4cade01b68
Admin <admin@MacBook-Pro.local>
2018-09-24 18:11:03 +0300

    second task first home ex

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class Test {
 
-    public static void main(String[] args) {
-        double a = 1.45d;
-        double b = 7.4;
+    public static String getPriseOfPurchase(double prise, double waight) {
+         String a = Double.toString(prise * waight);
+         return a;
+    }
 
-        System.out.println(a+b);
+
+    public static void main(String[] args) {
+
+        double pechenkiPrise;
+        double konfetiPrise;
+
+        System.out.println("Please enter the price of 1 kg of pechenki and 1 kg of konfeti");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the price of 1 kg of pechenki");
+        pechenkiPrise = Double.parseDouble(sc.nextLine());
+        System.out.println("Please enter the price of 1 kg of konfeti");
+        konfetiPrise = Double.parseDouble(sc.nextLine());
+
+        String a = getPriseOfPurchase(pechenkiPrise, 0.3);
+        String b = getPriseOfPurchase(konfetiPrise, 0.4);
+        String c = getPriseOfPurchase(konfetiPrise, 6);
+        String d = getPriseOfPurchase(pechenkiPrise, 5.4);
+
+
+        System.out.printf("the price of 0.3 kg pechenki: %s %n", a);
+        System.out.printf("the price of0.4 kg pechenki: %s %n", b);
+        System.out.printf("the prise of 6 kg konfeti is %s and 5.4 kg pechenki is %s", c, d);
+
     }
 
 }
---
+++ src/main/java/firsttask/Second.java
@@ -0,0 +1,34 @@
+package firsttask;
+
+public class Second {
+
+
+    static  int num = 45686;
+
+    public static void main(String[] args) {
+
+
+//        System.out.println(num%10);
+//        num = num /10;
+//        System.out.println(num%10);
+//        num = num /10;
+//        System.out.println(num%10);
+//        num = num /10;
+//        System.out.println(num%10);
+//        num = num /10;
+//        System.out.println(num%10);
+//        num = num /10;
+
+       while (num > 0 ) {
+           System.out.println(num%10);
+           num = num /10;
+       }
+
+
+
+
+
+
+
+    }
+}
