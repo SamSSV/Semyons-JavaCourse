78f501396524003d338d7f714e698c75d421f182
Admin <admin@MacBook-Pro.local>
2018-09-27 13:48:01 +0300

    done 2 task of 2 assigment

--- src/main/java/org/home/assigment2/SecondTask.java
+++ src/main/java/org/home/assigment2/SecondTask.java
@@ -2,30 +2,36 @@ package org.home.assigment2;
 
 public class SecondTask {
 
-    static int max;
-    static int min;
+    static int max, min;
 
-    private static long defaultNumber = 10454;
+    private static long defaultNumber = 91037810;
 
+
+    /*
+    Method compares last two digits in loop and defines min and max values
+    */
     public static void compareDigits(long num) {
+
         while (defaultNumber > 0) {
 
-            if (defaultNumber > 10) {
-
-                int a = (defaultNumber % 10 == 0) ? 0 : (int) defaultNumber % 10;
+            if (defaultNumber > 9) {
+                int percentOfNum = (int)defaultNumber % 10;
+                int a = (percentOfNum == 0) ? 0 : (int) percentOfNum;
                 defaultNumber = defaultNumber / 10;
-                int b = (defaultNumber % 10 == 0) ? 0 : (int) defaultNumber % 10;
+                percentOfNum = (int)defaultNumber % 10;
+                int b = (percentOfNum == 0) ? 0 : (int) percentOfNum;
 
                 int midMax = a > b ? a : b;
                 int midMin = a < b ? a : b;
 
                 max = midMax > max ? midMax : max;
-                min = midMin > min ? midMin : min;
-                
+                min = midMin < min ? midMin : min;
+
             } else {
                 int medium = (int) defaultNumber;
                 max = max > medium ? max : medium;
                 min = min < medium ? min : medium;
+                defaultNumber = defaultNumber / 10;
             }
 
         }
