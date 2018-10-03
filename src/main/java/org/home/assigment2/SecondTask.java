f4e904e1d746559e1be739716b58a8244050528f
Admin <admin@MacBook-Pro.local>
2018-10-03 18:58:08 +0300

    refactor 2-1

--- src/main/java/org/home/assigment2/SecondTask.java
+++ src/main/java/org/home/assigment2/SecondTask.java
@@ -5,13 +5,13 @@ public class SecondTask {
     static int max = 0;
     static int min = 10;
 
-    private static long defaultNumber = 90;
+    private static long defaultNumber = 87546321;
 
 
     /*
     Method compares last two digits in loop and defines min and max values
     */
-    public static void compareDigits(long num) {
+    /*public static void compareDigits(long num) {
 
         while (defaultNumber > 0) {
 
@@ -36,10 +36,22 @@ public class SecondTask {
             }
 
         }
+    }*/
+// after refactoring
+    public static void compareDigits1(long num) {
+
+        while (defaultNumber > 0) {
+            int a = (int) defaultNumber % 10;
+            defaultNumber = defaultNumber / 10;
+
+            max = (a > max) ? a : max;
+            min = (a < min) ? a : min;
+        }
     }
 
+
     public static void main(String[] args) {
-        compareDigits(defaultNumber);
+        compareDigits1(defaultNumber);
         System.out.println("max digit is: " + max);
         System.out.println("min digit is: " + min);
 
